aca8643915078e4d030c69000986e7dd1e8f9139
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-14 22:02:53 +0500

    квадрат это прямоугольник

---
+++ Problem1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            int N = Read("Введите размер массива");
+
+            int[] arr = new int[N];
+
+            Random rnd = new Random(); // надеюсь этот класс можно использвать
+            
+            Console.Write("массив: ");
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            int sum = 0;
+
+            for(int i = 0; i < N; i++) {
+                arr[i] = rnd.Next(0, 50);
+                min = Math.Min(arr[i], min);
+                max = Math.Max(arr[i], max);
+                sum += arr[i];
+                Console.Write($"{arr[i]} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"минимальный элемент: {min}");
+            Console.WriteLine($"максимальный элемент: {max}");
+            Console.WriteLine($"сумма элементов: {sum}");
+            Console.WriteLine($"Среднее арифметическое элементов: {((double)sum) / N}");
+            Console.Write($"Нечётные элементы: ");
+
+            foreach(int i in arr) 
+                if(i % 2 == 1) 
+                    Console.Write($"{i} ");
+                
+        }
+        static int Read(string varName) {
+            Console.Write($"{varName}: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Problem2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Problem2 {
+    class Program {
+        static void Main(string[] args) {
+            int N = Read("Размер массива");
+            int[] arr = new int[N];
+            int[] revArr = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                arr[i] = Read($"элемент {i + 1}");
+                revArr[N - i - 1] = arr[i];
+            }
+            
+            System.Console.Write("Reverse array: ");
+            foreach(int i in revArr) {
+                System.Console.Write($"{i} ");
+            }
+        }
+
+        static int Read(string varName) {
+            Console.Write($"{varName}: ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
